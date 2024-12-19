70c2e74ae1b03e1b8d4b57aa221724b6ecd30c99
satzch <gamingsplay240@gmail.com>
2024-12-19 20:57:07 +0530

    load img in canvas

--- js/script.js
+++ js/script.js
@@ -2,11 +2,29 @@
 const original_img = document.getElementById("original");
 const diff_img = document.getElementById("diff");
 
+// Get the canvas contexts
+const og_ctx = original_img.getContext("2d");
+const df_ctx = diff_img.getContext("2d");
 
 // Load the image
 function loadImages() {
-    original_img.src = game_data.images.image1;
-    diff_img.src = game_data.images.image2;
+    const og_img = new Image();
+    const df_img = new Image();
+
+    og_img.src = game_data.images.image1;
+    df_img.src = game_data.images.image2;
+
+    og_img.onload = () => {
+        original_img.width = og_img.naturalWidth;
+        original_img.height = og_img.naturalHeight;
+        og_ctx.drawImage(og_img, 0, 0);
+    }
+
+    df_img.onload = () => {
+        diff_img.width = df_img.naturalWidth;
+        diff_img.height = df_img.naturalHeight;
+        df_ctx.drawImage(df_img, 0, 0);
+    }
 }
 
 loadImages();
